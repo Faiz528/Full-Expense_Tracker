6270b52fd8159d2accf347c480bdbebc37df7632
MD FAIZUDDIN <84907848+Faiz528@users.noreply.github.com>
2023-06-03 08:04:28 +0530

    Encrypted password

--- login/control/log.js
+++ login/control/log.js
@@ -1,5 +1,5 @@
 const User = require('../../model/user')
-
+const bcrypt = require('bcrypt')
 exports.VerifyUser= async(req,res,next)=>{
   try {
     const { email, pass } = req.body; // Destructure email and pass from req.body
@@ -7,15 +7,20 @@ exports.VerifyUser= async(req,res,next)=>{
 
     const user = await User.findOne({ where: { Email: email } });
     console.log(user);
-
+     
     if (!user) {
       return res.status(404).json("User not found"); // User not found, return null
     } else {
-      if (user.Password !== pass) {
-        return res.status(401).json("User not authorised");
+      bcrypt.compare(pass , user.Password,(err,result)=>{
+        if(err)
+        res.status(500).json("Something went wrong")
+      
+      if (result== true) {
+        return res.status(200).json({message:"Logged in Successfully"});
       } else {
-        return res.status(200).json(user);
+        return res.status(401).json("User not authorised");
       }
+    })
     }
   }
   catch(err){
