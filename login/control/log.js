7944c4369cf7b954f3247cf8d5d6a4e94dfa8984
MD FAIZUDDIN <84907848+Faiz528@users.noreply.github.com>
2023-06-05 15:58:36 +0530

    Authorisation added

--- login/control/log.js
+++ login/control/log.js
@@ -12,7 +12,40 @@ exports.VerifyUser= async(req,res,next)=>{
       return res.status(404).json("User not found"); // User not found, return null
     } else {
       bcrypt.compare(pass , user.Password,(err,result)=>{
+        if(err)const User = require('../../model/user')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+exports.VerifyUser= async(req,res,next)=>{
+  try {
+    const { email, pass } = req.body; // Destructure email and pass from req.body
+    console.log(req.body);
+
+    const user = await User.findOne({ where: { Email: email } });
+    const id = user.id
+    console.log(id)
+    console.log(user);
+     
+    if (!user) {
+      return res.status(404).json("User not found"); // User not found, return null
+    } else {
+      bcrypt.compare(pass , user.Password,(err,result)=>{
         if(err)
+        res.status(500).json("Something went wrong")
+      
+      if (result== true) {
+        const token = jwt.sign({id},'secret')
+        return res.status(200).json({message :"Loogged in Succesfully" ,userid:id , token:token})
+      } else {
+        return res.status(401).json("User not authorised");
+      }
+    })
+    }
+  }
+  catch(err){
+    console.log(err)
+  }
+} 
+//
         res.status(500).json("Something went wrong")
       
       if (result== true) {
