0ff07c5d41207d0aed8cf1962a5ef2ef6d54c6ff
MD FAIZUDDIN <84907848+Faiz528@users.noreply.github.com>
2023-06-08 10:05:56 +0530

    Connecting User, Order and expense table

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
+require('dotenv').config();
 const express = require('express')
 const app = express()
 const Parser = require('body-parser')
 
 const sequelize = require('./util/database')
 const cors = require('cors')
-
-
+const User = require('./model/user')
+const Expenses = require('./model/expense')
+const Order = require('./model/order')
 
 app.use(cors())
 app.use(Parser.json({extended:false}))
@@ -14,6 +16,17 @@ const Signup = require('./sign up/route/sign')
 app.use(Signup)
 const Login = require('./login/route/log')
 app.use(Login)
+const Expense = require('./expenses/route/expense')
+app.use(Expense)
+
+
+
+User.hasMany(Expenses)
+Expenses.belongsTo(User)
+
+User.hasMany(Order)
+Order.belongsTo(User)
+
 
 sequelize.
 sync().then(result=>{
