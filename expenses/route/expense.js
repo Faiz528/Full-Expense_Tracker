8095070e75046ca05bba1c23d228d1f89ef0442b
MD FAIZUDDIN <84907848+Faiz528@users.noreply.github.com>
2023-06-09 15:45:26 +0530

    Leader route updated

--- expenses/route/expense.js
+++ expenses/route/expense.js
@@ -5,7 +5,7 @@ const route = express.Router()
 const Control = require('../controlller/expense')
 const Premium = require('../controlller/purchase')
 route.post('/add',Control.PostExpense)
-
+route.get('/leaderlist',Control.Getleader)
 route.get('/premium',authenticates.authenticate, Premium.Premium)
 route.post('/updatetransactionstatus',authenticates.authenticate, Premium.updateTransaction)
 
@@ -16,6 +16,6 @@ route.get('/add' ,Control.GetExpense )
 route.get('/edit/:id',Control.EditExpense)
 
 route.delete('/delete/:id',Control.DeleteExpense)
-
+route.get('/totals/:sum',authenticates.authenticate,Control.Gettotal)
 
 module.exports = route
