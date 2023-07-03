fca174ca87733c3488e248b55dab6d5f44fbc844
MD FAIZUDDIN <84907848+Faiz528@users.noreply.github.com>
2023-07-03 14:41:17 +0530

    Update database.js witn .env

--- util/database.js
+++ util/database.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize')
-const sequelize = new Sequelize('Expense_tracker','root','Gate@2022',{
+const sequelize = new Sequelize(process.env.DB_name,  process.env.DB_username,  process.env.DB_password,{
     dialect : 'mysql',
-    host :'localhost'
+    host :process.env.DB_host
 })
 
 module.exports = sequelize
